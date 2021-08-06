41dbcc8f0378e0a2656cbd97a818d1ee379050c2
inthehouse0513 <hiroshi.inthehouse0513@gmail.com>
2021-08-06 09:34:56 +0900

    よくわからない

--- src/esp8266_ardutracerPC_v01.ino
+++ src/esp8266_ardutracerPC_v01.ino
@@ -1,9 +1,12 @@
 //2021.06.02
 //Auther Hiroshi Yoshida
 //Ardutracer受信モジュール用のプログラム
+//こちらがアクセスポイント
 
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #ifndef STASSID
 #define STASSID "esptracer"
@@ -11,9 +14,9 @@
 #define num 255
 #endif
 
-//IPAddress myIP(192,168,1,101);  // 自分のIPアドレス
-//IPAddress gateway(192,168,1,1);  // GW
-//IPAddress subnet(255,255,255,0);  // subnet
+IPAddress myIP(192,168,1,101);  // 自分のIPアドレス
+IPAddress gateway(192,168,1,1);  // GW
+IPAddress subnet(255,255,255,0);  // subnet
 char wifibuff[num];       //受信するデータ
 unsigned int localPort = 8100;      // local port to listen on
 WiFiUDP Udp;
@@ -25,11 +28,13 @@ void setup() {
   digitalWrite(LED_BUILTIN,LOW);
   delay(100);
 
-  Serial.begin(115200);
-//  WiFi.config(myIP,gateway,subnet);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(STASSID, STAPSK);
+  WiFi.mode(WIFI_AP);   //WiFi.mode(WIFI_STA);
+  WiFi.softAPConfig(myIP, WiFi.gatewayIP(), subnet); //WiFi.config(myIP,gateway,subnet);
+  delay(100);
+  WiFi.softAP(STASSID, STAPSK); //WiFi.begin(STASSID, STAPSK);
 
+  Serial.begin(115200);
+/*
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -37,30 +42,38 @@ void setup() {
   }
   Serial.print("Connected at");
   Serial.println(WiFi.localIP());
+*/
   Udp.begin(localPort);
 
+  delay(200);
+  Serial.print("\n") ;
+  Serial.print("esptracer START") ;
   //ビルトインＬＥＤの初期化のための消灯
+  delay(200);
   digitalWrite(LED_BUILTIN,HIGH);
 }
 
 
 void loop() {
+
   //受信データ　パケットサイズの取得
   int packetSize = Udp.parsePacket();
 
   //udpパケット監視　正ならばif内処理の実行
-  if(packetSize >0 ){
+  if(packetSize > 0){
     Udp.read(wifibuff, packetSize);   //データ受信
-    Udp.flush();
+    Udp.flush();  //これを入れるとネットワークに0.0.0.0あての謎のメッセージを発信する
     Serial.print(wifibuff);
-    Serial.print("...");
-    Serial.println(packetSize);
+    Serial.print("..");
+    Serial.println(millis());
+ //   Serial.print("..");
+ //   Serial.println(packetSize);
 
     //転送後にバッファをクリア
     for(int i=0 ; i<num ; i++){
       wifibuff[i] = 0;
     }
+    delay(3);
+
   }
-
-
 }
